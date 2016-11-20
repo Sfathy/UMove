cbfa9fecb8c042aa89dd7f3e757ac4a415838866
hossam300 <semsem9000@gmail.com>
2016-11-20 14:32:47 +0200

    files

---
+++ BasePage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace UMoveNew
+{
+    public class BasePage : Page
+    {
+        protected override void InitializeCulture()
+        {
+            HttpCookie mylang = Request.Cookies["lang"];
+
+            if (Request.Cookies["lang"] != null)
+            {
+                string lang = Convert.ToString(mylang.Values["lang"].ToString());
+                Culture = lang;
+                UICulture = lang;
+            }
+            base.InitializeCulture();
+        }
+    }
+}
