8cdda8fb44ca61184da332a8f73e5a8439198aeb
hossam300 <semsem9000@gmail.com>
2016-12-07 20:13:13 +0200

    bin

--- Models/paymentMethodDetails.cs
+++ Models/paymentMethodDetails.cs
@@ -7,6 +7,7 @@ namespace UMoveNew.Models
 {
     public class paymentMethodDetails
     {
+        public int ID { get; set; }
         public int UserID { get; set; }
         public string MethodName { get; set; }
         public DateTime ExpiryDate { get; set; }
