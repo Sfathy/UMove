236c703c5008a51bcc25d9fabe49bf2ec3b0d64a
hossam300 <semsem9000@gmail.com>
2016-12-14 11:44:13 +0200

    adding notifcation Model

---
+++ Models/Notification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UMoveNew.Models
+{
+    public class Notification
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public DateTime NotDate { get; set; }
+        public string Description { get; set; }
+    }
+}
