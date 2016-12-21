0717fa5fe7e0a2900e9e68b6318e5bf7009032d8
hossam300 <semsem9000@gmail.com>
2016-12-21 21:32:27 +0200

    user and Driver Rate

--- Models/RateDriver.cs
+++ Models/RateDriver.cs
@@ -10,7 +10,7 @@ namespace UMoveNew.Models
         public int ID { get; set; }
         public int TripID { get; set; }
         public int DriverID { get; set; }
-        public int Rate { get; set; }
+        public decimal Rate { get; set; }
 
         public string Name { get; set; }
         public int UserID { get; set; }
